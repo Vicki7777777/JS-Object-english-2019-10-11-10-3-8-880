e4ddbdaab2268cab69ee1cf55248e743cb34df8c
Vicki7777777 <43415417+Vicki7777777@users.noreply.github.com>
2020-06-21 18:11:29 +0800

    Q1-JS-Object
    Q1-JS-Object

---
+++ Q1.js
@@ -0,0 +1,5 @@
+var user = new Object();       //Create a null object and set its variable name as user;
+user.name = "John";				//Add a name attribute and set its value as John;
+user.surname = "Mike";			//Add a surname attribute and set its value as Mike;
+user.name = "Peter";			//Modify the value of name attribute as Peter;
+delete user.name;				//Delete name attribute of user.
